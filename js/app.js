37ea57e1db319024aed7b820c0822d8301245781
paula_ <75187606+polly-em@users.noreply.github.com>
2021-02-22 22:26:11 -0500

    Update app.js
    Added code snippet to enable search filter

--- js/app.js
+++ js/app.js
@@ -1,2 +1,20 @@
 baguetteBox.run('.gallery');
-const search = new Filter('search', 'data-caption');
+const search = document.querySelector('#search');
+
+//Code Snippet from
+//https://www.geeksforgeeks.org/search-bar-using-html-css-and-javascript/
+
+function search_photos() { 
+    let input = document.getElementById('searchbar').value 
+    input = input.toLowerCase(); 
+    let img = document.getElementsByClassName('photos'); 
+      
+    for (i = 0; i < img.length; i++) {  
+        if (!img[i].innerHTML.toLowerCase().includes(input)) { 
+            img[i].style.display="none"; 
+        } 
+        else { 
+            img[i].style.display="list-item";                  
+        } 
+    } 
+} 
